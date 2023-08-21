c1696f72972bff3a9702c91d1454d27a342016e3
Esraa Alaa Elden <122882067+Esraaalaaelden@users.noreply.github.com>
2023-08-22 00:00:18 +0300

    about_app

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -9,14 +9,66 @@ class AboutAppPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var cubit = LayoutCubit.get(context);
+    return  Container(
+      padding: EdgeInsets.all(8),
+      child:
+      Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
 
-    return  Column(
-      children: [
-        Container(
-            padding: const EdgeInsets.all(20),
-            child: Center(child: Text('About App Page',style: TextStyle(color: buttonColor,fontSize: 30),))
+            SizedBox(height: 10),
+             Image(image: AssetImage('assets/sww.png'),
+              //width: 200,
+              //height: 200,
+            ),
+            SizedBox(height:50.0),
+            Container(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Our Shop Offers a Wide Range of High-Quality Products and Services To Meet The Needs of Our Customers.',
+                 style: TextStyle(fontSize: 20
+                     ,fontWeight: FontWeight.w400),
+                )
+            ),
+            Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Products:',
+                      style: TextStyle(fontSize: 25
+                          ,fontWeight: FontWeight.bold,
+                      color:buttonColor ),
+                    ),
+                    SizedBox(height: 8),
+                    ListTile(
+                      leading: Icon(Icons.check),
+                      title: Text('Ice Cream:We Always Use Fresh And Natural Fruits'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.check),
+                      title: Text('Waffel:  We master the art of making waffles'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.check),
+                      title: Text('Cake: We offer the best birthday cakes to suit all tastes'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.check),
+                      title: Text('Drinks:We Have Some Of The Best tumblers for hot drinks and cold'),
+                    ),
+                  ],
+                )
+            )
+          ],
         ),
-      ],
+      ),
     );
   }
 }
+
+
